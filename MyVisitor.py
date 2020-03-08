0cd4ed016ec4747a02b67a7dc44533564a137318
Marie Akhramenko <triangletrue@yahoo.com>
2020-03-08 15:54:00 +0300

    Add parse and array declaration

--- MyVisitor.py
+++ MyVisitor.py
@@ -21,27 +21,36 @@ class MyVisitor(xml_compilerVisitor):
 
     # root.set("hello", "Huhu")
     def visitAppend_atr(self, ctx):
-        tag = self.memory[str(ctx.ID()[0])]
+        # tag = self.memory[str(ctx.ID()[0])]
         attribute = self.memory[str(ctx.ID()[1])]
-        tag.set(list(attribute.keys())[0], list(attribute.values())[0])
+        # tag.set(list(attribute.keys())[0], list(attribute.values())[0])
         self.file.write("{}.set(\"{}\", \"{}\")\n".format(str(ctx.ID()[0]), list(attribute.keys())[0], list(attribute.values())[0]))
 
     # child.text = 'some text'
     def visitAdd_text(self, ctx):
-        tag = self.memory[str(ctx.ID()[0])]
-        tag.text = str(ctx.ID()[1])
+        # tag = self.memory[str(ctx.ID()[0])]
+        # tag.text = str(ctx.ID()[1])
         self.file.write("{}.text = '{}'\n".format(str(ctx.ID()[0]), str(ctx.ID()[1])))
 
     # root.append(child)
     def visitAppend_tag(self, ctx):
-        parent = self.memory[str(ctx.ID()[0])]
-        child = self.memory[str(ctx.ID()[1])]
-        parent.append(child)
+        # parent = self.memory[str(ctx.ID()[0])]
+        # child = self.memory[str(ctx.ID()[1])]
+        # parent.append(child)
         self.file.write("{}.append({})\n".format(str(ctx.ID()[0]), str(ctx.ID()[1])))
 
     def visitGen_file(self, ctx):
-        filename = str(ctx.ID()[1]) + ".xml"
-        root = self.memory[str(ctx.ID()[0])]
-        with open(filename, 'wb') as doc:
-            doc.write(etree.tostring(root, pretty_print = True))
+        # filename = str(ctx.ID()[1]) + ".xml"
+        # root = self.memory[str(ctx.ID()[0])]
+        # with open(filename, 'wb') as doc:
+        #     doc.write(etree.tostring(root, pretty_print = True))
         self.file.write("with open(\"{}\", 'wb') as doc:\n    doc.write(etree.tostring({}, pretty_print = True))\n".format(str(ctx.ID()[1]) + ".xml", str(ctx.ID()[0])))
+
+    def visitParse_file(self, ctx):
+        filename = str(ctx.FILENAME())
+        root = str(ctx.ID())
+        self.file.write("tree = etree.parse(\"{}\")\n{} = tree.getroot()\n".format(filename, root))
+
+    def visitDeclare_array(self, ctx):
+        array = str(ctx.ID())
+        self.file.write("{} = []\n".format(array))
