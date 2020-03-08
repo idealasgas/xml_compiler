84993f900a69b7fbce985d5e9a8b7bbcbbf5c5ec
Marie Akhramenko <triangletrue@yahoo.com>
2020-03-08 15:19:38 +0300

    Generates file

--- MyVisitor.py
+++ MyVisitor.py
@@ -5,13 +5,14 @@ from xml_compilerParser import xml_compilerParser
 class MyVisitor(xml_compilerVisitor):
     def __init__(self):
         self.memory = {}
-        self.file = '';
+        self.file = open("file.py", "w+")
+        self.file.write("from lxml import etree\n")
 
     # root = etree.Element('root')
     def visitTag_assignment(self, ctx):
         value = etree.Element(str(ctx.ID()[1]))
         self.memory[str(ctx.ID()[0])] = value
-        print("{} = etree.Element('{}')".format(str(ctx.ID()[0]), str(ctx.ID()[1])))
+        self.file.write("{} = etree.Element('{}')\n".format(str(ctx.ID()[0]), str(ctx.ID()[1])))
 
     def visitAttr_assignment(self, ctx):
         value = {}
@@ -23,24 +24,24 @@ class MyVisitor(xml_compilerVisitor):
         tag = self.memory[str(ctx.ID()[0])]
         attribute = self.memory[str(ctx.ID()[1])]
         tag.set(list(attribute.keys())[0], list(attribute.values())[0])
-        print("{}.set(\"{}\", \"{}\")".format(str(ctx.ID()[0]), list(attribute.keys())[0], list(attribute.values())[0]))
+        self.file.write("{}.set(\"{}\", \"{}\")\n".format(str(ctx.ID()[0]), list(attribute.keys())[0], list(attribute.values())[0]))
 
     # child.text = 'some text'
     def visitAdd_text(self, ctx):
         tag = self.memory[str(ctx.ID()[0])]
         tag.text = str(ctx.ID()[1])
-        print("{}.text = '{}'".format(str(ctx.ID()[0]), str(ctx.ID()[1])))
+        self.file.write("{}.text = '{}'\n".format(str(ctx.ID()[0]), str(ctx.ID()[1])))
 
     # root.append(child)
     def visitAppend_tag(self, ctx):
         parent = self.memory[str(ctx.ID()[0])]
         child = self.memory[str(ctx.ID()[1])]
         parent.append(child)
-        print("{}.append({})".format(str(ctx.ID()[0]), str(ctx.ID()[1])))
+        self.file.write("{}.append({})\n".format(str(ctx.ID()[0]), str(ctx.ID()[1])))
 
     def visitGen_file(self, ctx):
         filename = str(ctx.ID()[1]) + ".xml"
         root = self.memory[str(ctx.ID()[0])]
         with open(filename, 'wb') as doc:
             doc.write(etree.tostring(root, pretty_print = True))
-        # print("filename = \"{}\" + \".xml\"\nroot = {}")
+        self.file.write("with open(\"{}\", 'wb') as doc:\n    doc.write(etree.tostring({}, pretty_print = True))\n".format(str(ctx.ID()[1]) + ".xml", str(ctx.ID()[0])))
