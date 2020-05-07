d3936b403ed4d560565f4b354562ed66300c2140
Marie Akhramenko <triangletrue@yahoo.com>
2020-05-07 20:22:15 +0300

    Add script

---
+++ compile_please.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+C:\Users\Acer\AppData\Local\Programs\Python\Python37\python.exe xml-compiler.py %1
+PAUSE
--- xml-compiler.py
+++ xml-compiler.py
@@ -7,19 +7,23 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        filename = sys.argv[1]
-        input_stream = FileStream(filename)
+        try:
+            filename = sys.argv[1]
+            input_stream = FileStream(filename)
 
-        lexer = xml_compilerLexer(input_stream)
-        token_stream = CommonTokenStream(lexer)
-        parser = xml_compilerParser(token_stream)
-        tree = parser.prog()
+            lexer = xml_compilerLexer(input_stream)
+            token_stream = CommonTokenStream(lexer)
+            parser = xml_compilerParser(token_stream)
+            tree = parser.prog()
 
-        file = open('file.py', 'w+')
-        function_visitor = FunctionVisitor(file)
-        function_visitor.visit(tree)
-        visitor = MyVisitor(file)
-        visitor.visit(tree)
-        file.close()
+            file = open('file.py', 'w+')
+            function_visitor = FunctionVisitor(file)
+            function_visitor.visit(tree)
+            visitor = MyVisitor(file)
+            visitor.visit(tree)
+            file.close()
+            print('Your python script is in file.py')
+        except FileNotFoundError:
+            print('Make sure provided file does exist')
     else:
         print('Provide filename')
